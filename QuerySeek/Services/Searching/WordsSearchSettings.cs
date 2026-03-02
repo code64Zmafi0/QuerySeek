cf7a92c276b24d68cc84a92428f896ea6e3b89e4
code64ZmafI0 <49468734+code64ZmafI0@users.noreply.github.com>
2026-03-03 03:06:07 +0500

    settings default

--- QuerySeek/Services/Searching/WordsSearchSettings.cs
+++ QuerySeek/Services/Searching/WordsSearchSettings.cs
@@ -3,12 +3,12 @@ namespace QuerySeek.Services.Searching;
 public record WordsSearchSettings(
     int MaxCheckingWordsCount,
     int SearchedWordsToStopProcess,
-    int WordsSearchDictionaryPreallocate,
+    int WordsSearchDictionaryPreallocate = 350_000,
     double Similarity = 0.5)
 {
-    public static readonly WordsSearchSettings Default = new(500, 6, 350_000);
+    public static readonly WordsSearchSettings Default = new(600, 6);
 
-    public static readonly WordsSearchSettings Fast = new(200, 2, 200_000);
+    public static readonly WordsSearchSettings Fast = new(200, 2, 300_000, 0.7);
 
     public WordsSearchManager GetWordsSearchManager() => new(SearchedWordsToStopProcess);
 }
